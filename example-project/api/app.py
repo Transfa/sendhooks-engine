dfa74af2148e4cc3551cd61bc7ae68ed81f1f53f
koladev <onaelmangabo@gmail.com>
2023-09-09 23:15:01 +0100

    🚧 adding exemple project

---
+++ example-project/api/app.py
@@ -0,0 +1,25 @@
+# app.py
+from flask import Flask, request, jsonify
+import redis
+import json
+
+app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379, db=0)
+
+@app.route('/api/send', methods=['POST'])
+def send_data():
+    payload = request.json
+    print(payload)
+
+    r.publish('hooks', json.dumps(payload))
+    return jsonify({"status": "sent to channel"})
+
+@app.route('/webhook', methods=['POST'])
+def webhook_endpoint():
+    data = request.json
+    # Process the webhook data as needed
+    print(data)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
